9045ef16ddd2866e1d2da31acee2b21fac97b025
zhanglei <13588462013@qq.com>
2025-03-25 16:50:11 +0800

    feat:修改原生代码，兼容rn0.76版本

---
+++ WheelCurvedPicker.android.js
@@ -0,0 +1,77 @@
+'use strict';
+
+import React from 'react';
+import PropTypes from 'prop-types'
+import {
+  View,
+  ColorPropType,
+  requireNativeComponent,
+} from 'react-native';
+
+const defaultItemStyle = { color: 'white', fontSize: 26 };
+
+const WheelCurvedPickerNativeInterface = {
+  name: 'WheelCurvedPicker',
+}
+
+const WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker', WheelCurvedPickerNativeInterface);
+
+class WheelCurvedPicker extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = this._stateFromProps(props)
+  }
+
+  static defaultProps = {
+    itemStyle : {color:"white", fontSize:26},
+    itemSpace: 20
+  }
+
+  UNSAFE_componentWillReceiveProps (props) {
+    this.setState(this._stateFromProps(props));
+  }
+
+  _stateFromProps (props) {
+    var selectedIndex = 0;
+    var items = [];
+    React.Children.forEach(props.children, function (child, index) {
+      if (child.props.value === props.selectedValue) {
+        selectedIndex = index;
+      }
+      items.push({value: child.props.value, label: child.props.label});
+    });
+
+    var textSize = props.itemStyle.fontSize
+    var textColor = props.itemStyle.color
+
+    return {selectedIndex, items, textSize, textColor};
+  }
+
+  _onValueChange = (e) => {
+    if (this.props.onValueChange) {
+      this.props.onValueChange(e.nativeEvent.data);
+    }
+  }
+
+  render() {
+    return <WheelCurvedPickerNative
+      {...this.props}
+      onValueChange={this._onValueChange}
+      data={this.state.items}
+      textColor={this.state.textColor}
+      textSize={this.state.textSize}
+      selectedIndex={parseInt(this.state.selectedIndex)} />;
+  }
+}
+
+class Item extends React.Component {
+  render () {
+    // These items don't get rendered directly.
+    return null;
+  }
+}
+
+WheelCurvedPicker.Item = Item;
+
+module.exports = WheelCurvedPicker;
